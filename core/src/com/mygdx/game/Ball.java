2ec3304adf09c58065b23bad71c53418fdee57dd
Sverre Nystad <sverrnys@stud.ntnu.no>
2024-03-02 01:35:04 +0100

    Feat: remove unused launchers and correct desktop launcher

--- core/src/com/mygdx/game/Ball.java
+++ core/src/com/mygdx/game/Ball.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
 
 public class Ball {
-    private Circle bounds; // To handle the position and collision
+    private Circle bounds;
     private Vector2 velocity;
     private Texture texture = new Texture("Ball.png");
 
